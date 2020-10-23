18f83fc3239e7b39cfd22937cdda03eee405c605
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-10-23 17:21:08 +0900

    ログインしていたらTweetsページに飛ばす

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,14 @@
 class TopsController < ApplicationController
+  before_action :redirect
   def index
     
+  end
+
+  private
+
+  def redirect
+    if student_signed_in? || teacher_signed_in?
+      redirect_to tweets_path
+    end
   end
 end
