3f3bfaf45f455e3da2faf3fd048091d253fca7c1
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-10-24 11:14:36 +0900

    各モデルのアソシエーション記述
    mount_uploaderも記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  mount_uploader :movie, PostUploader
+  belongs_to :teacher
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ApplicationRecord
+  belongs_to :teacher
+  belongs_to :student
 end
