8ac1c4361811d8329dfe62cf7efc98b9e8a66094
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-02 18:03:58 +0900

    jQuery読み込みの記述

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+// jqueryを読み込む記述
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+// jqueryを読み込む記述
+
 module.exports = environment
