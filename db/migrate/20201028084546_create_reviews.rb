2b351b9bf2c332e523dd464f6234f033bdf5d8f2
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-03 13:05:10 +0900

    レビューテーブル作成

--- db/migrate/20201028084546_create_reviews.rb
+++ db/migrate/20201028084546_create_reviews.rb
@@ -1,7 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.text   :difficult
+      t.text   :text,        null: false
+      t.references :teacher, foreign_key: true
+      t.references :student, foreign_key: true
+      t.references :post,    foreign_key: true
       t.timestamps
     end
   end
