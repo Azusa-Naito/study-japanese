aaab6fc39e2eeae60f7f1a1bc27bcb6c71a3951f
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-03 13:05:23 +0900

    reviewコントローラー記述

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @review = Review.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to post_path(@post.id)
+  end
+
+  private
+
+  def review_params
+    if student_signed_in?
+      params.require(:review).permit(:difficult, :text).merge(student_id: current_student.id, post_id: @post.id)
+    elsif teacher_signed_in?
+      params.require(:review).permit(:difficult, :text).merge(teacher_id: current_teacher.id, post_id: @post.id)
+    else
+      params.require(:review).permit(:difficult, :text).merge(post_id: @post.id)
+    end
+  end
 end
